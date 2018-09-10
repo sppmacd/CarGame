81d13f593ed48237f4314c58a4794f1c17a0dfab
sppmacd <sppmacd@gmail.com>
2018-09-10 16:58:29 +0200

    fixed button image #2

--- ButtonImage.cpp
+++ ButtonImage.cpp
@@ -27,7 +27,7 @@ void ButtonImage::draw(RenderWindow* wnd)
 	Sprite spr(*this->img, IntRect(Vector2i(0,0),Vector2i(this->getSize())));
 	spr.setPosition(this->getPos() + this->getSize() / 2.f);
 	spr.setOrigin(this->getSize() / 2.f);
-	if (!enabled) spr.setColor(Color(128, 128, 128));ge
-	spr.setScale(isMouseOver ? Vector2f(0.95f, 0.95f) : Vector2f(0.9f, 0.9f));
+	if (!enabled) spr.setColor(Color(128, 128, 128));
+	spr.setScale((isMouseOver && enabled) ? Vector2f(0.95f, 0.95f) : Vector2f(0.9f, 0.9f));
 	wnd->draw(spr);
 }
