5a47599888e120f17515ffa7007fda9236c6fb0a
sppmacd <sppmacd@gmail.com>
2019-07-12 10:02:43 +0200

    Fixed file tree: the code is now in /src directory

---
+++ src/version.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#define CG_VERSION "beta 0.2 v190701 [snapshot]"
+#define CG_VERSION_NO 0,2,19,0701
--- version.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define CG_VERSION "beta 0.2 v190701"
-#define CG_VERSION_NO 0,2,19,0701
