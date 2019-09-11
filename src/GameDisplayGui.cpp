5574a5ab7e4455ed87a5560698377031f7e9fa90
SITSGameDev <42967349+sppmacd@users.noreply.github.com>
2019-09-11 11:29:17 +0200

    Display stat description if player is new

--- src/GameDisplayGui.cpp
+++ src/GameDisplayGui.cpp
@@ -16,6 +16,16 @@ void GameDisplay::drawStat(int x, int y, String texture, long val, int animTick)
     t1.move(t1.getLocalBounds().width / 2.f, t1.getLocalBounds().height / 2.f);
     t1.setScale(animTick / 30.f + 1.f, animTick / 30.f + 1.f);
     this->renderWnd->draw(t1);
+if(Game::instance->isNewPlayer) 
+{
+t1.move(0.f, 30.f);
+t1.setOrigin(0.f, 0.f);
+t1.setFillColor(Color::Cyan);
+t1.setCharacterSize(20);
+t1.setScale(1.f, 1.f);
+t1.setString(Game::instance->translation.get("stat."+texture));
+renderWnd->draw(t1);
+}
 }
 
 void GameDisplay::drawLoading(sf::RenderWindow* wnd)
