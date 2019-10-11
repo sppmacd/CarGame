dd75b37a95982f1e720f8434fb07777c55d678cc
SITSGameDev <42967349+sppmacd@users.noreply.github.com>
2019-10-11 19:38:27 +0200

    doc

--- src/ButtonCircle.h
+++ src/ButtonCircle.h
@@ -5,13 +5,30 @@
 
 using namespace sf;
 
+// A class to create circular button with image in GUI.
 class ButtonCircle : public ButtonImage
 {
+    // Circle radius.
     float bRadius;
 public:
     ButtonCircle() {}
+    
+    // %gui - The parent GUI
+    // %radius - Button radius
+    // %pos - Button position (left up corner)
+    // %imageName - Image texture name (loaded from "res")
+    // %id - ID used in onClick()
     ButtonCircle(Gui* gui, float radius, Vector2f pos, string imageName, int id);
+    
+    // Sets the new radius for button.
+    // %radius - The new radius
     void setRadius(float radius);
+    
+    // Draw button to window.
+    // %wnd - The SFML RenderWindow.
     void draw(RenderWindow& wnd);
+    
+    // \returns <true> if %pos belongs to widget area, <false> otherwise.
+    // %pos - The checked position.
     bool isClicked(Vector2f pos);
 };
