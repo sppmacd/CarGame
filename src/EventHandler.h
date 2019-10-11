8a2220516af8e9546fbddefdcc4d5bcb3d9b9bd4
SITSGameDev <42967349+sppmacd@users.noreply.github.com>
2019-10-11 20:07:40 +0200

    doc

--- src/EventHandler.h
+++ src/EventHandler.h
@@ -14,21 +14,37 @@ typedef bool(*GameEventHandler)(GameEvent&, Game*);
 
 namespace EventHandlers
 {
+	// Called when the window close is requested.
 	bool onClose(Event event, Game* game);
+	
+	// Called when the mouse button is released.
 	bool onMouseButtonReleased(Event event, Game* game);
+	
+	// Called when the mouse wheel is scrolled.
 	bool onMouseWheelScrolled(Event event, Game* game);
+	
+	// Called when the keyboard key is pressed.
 	bool onKeyPressed(Event event, Game* game);
+	
+	// Not used.
 	SFML_DEPRECATED bool onGUIKeyPressed(Event event, Game* game);
 
 	// True if the car has to be spawned.
 	bool onCarSpawning(GameEvent& event, Game* game);
 }
 
+// Class holding game event handlers.
 class EventsHandler
 {
 public:
 	EventsHandler();
+	
+	// The singleton.
 	static EventsHandler* instance;
+	
+	// Game event registry. Types defined in <GameEvent.h>.
 	multimap<GameEvent::Type, GameEventHandler> registry;
+	
+	// Register new GameEvent. Types defined in <GameEvent.h>.
 	void registerGameEvent(GameEvent::Type event, GameEventHandler func);
 };
