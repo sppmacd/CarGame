64528e6007eba07c60a5f1c93701e80a53aa4f3a
sppmacd <sppmacd@gmail.com>
2020-05-04 11:55:40 +0200

    DebugLogger: changed log format
    * Added function to specifying log level (e.g. info, error)
    * Changed log format - added log level and log entry time.

--- src/DebugLogger.cpp
+++ src/DebugLogger.cpp
@@ -2,16 +2,40 @@
 
 #include <iostream>
 #include "Game.h"
+#include <sstream>
+#include <iomanip>
+
+std::string time2str()
+{
+    time_t _t = time(NULL);
+    tm* _tstr = localtime(&_t);
+    std::ostringstream _str;
+    _str << std::setfill('0')
+               << std::setw(4) << (_tstr->tm_year + 1900)
+        << "-" << std::setw(2) << _tstr->tm_mon
+        << "-" << std::setw(2) << _tstr->tm_mday
+        << " " << std::setw(2) << _tstr->tm_hour
+        << ":" << std::setw(2) << _tstr->tm_min
+        << ":" << std::setw(2) << _tstr->tm_sec
+        << " / " << clock();
+    return _str.str();
+}
 
 // TODO: change format
-void DebugLogger::log(std::string str, std::string logger)
+void DebugLogger::log(std::string str, std::string logger, std::string level)
 {
-    std::cout << logger << ": " << str << std::endl;
+    // Get the current time string.
+    time_t _t;
+    time(&_t);
+    std::string timeString = time2str();
+
+    // Display string on standard output.
+    std::cout << "{ " << timeString << " } [ " << level << " ]  " << logger << ":  " << str << std::endl;
 }
-void DebugLogger::logDbg(std::string str, std::string logger)
+void DebugLogger::logDbg(std::string str, std::string logger, std::string level)
 {
     if(!Game::instance || Game::instance->debug)
-        std::cout << "[DEBUG] " << logger << ": " << str << std::endl;
+        log(str, "--DEBUG-- " + logger, level);
 }
 
 
--- src/DebugLogger.hpp
+++ src/DebugLogger.hpp
@@ -5,9 +5,9 @@
 namespace DebugLogger
 {
     // Displays log entry on stdout.
-    void log(std::string str, std::string logger = "main");
+    void log(std::string str, std::string logger = "main", std::string level = "INFO");
 
     // Displays log entry on stdout, if it's enabled by Debug Mode
     // or this option is unknown (e.g. because of Game not created)
-    void logDbg(std::string str, std::string logger = "main");
+    void logDbg(std::string str, std::string logger = "main", std::string level = "DEBUG");
 };
